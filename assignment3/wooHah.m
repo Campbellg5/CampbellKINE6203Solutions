2319e47275cb198ec8002bd338eaa46c185aad63
Campbellg5 <143045816+Campbellg5@users.noreply.github.com>
2023-10-05 19:43:51 -0400

    completed A3

--- assignment3/wooHah.m
+++ assignment3/wooHah.m
@@ -3,9 +3,12 @@ function [textOut] = wooHah(num1,num2)
 %wooHah will take two number inputs then add those up. If the sum of those
 %two numbers is even it will say "Woo" if they are odd it will say "Hah".
 %Please input two numbers
-value1 = num1 + num2
 
+%Adding up num1 + num2
+value1 = num1 + num2
+%Checking if odd or even
 if (rem(value1,2)==0)
+   
     textOut = "Woo";
 else
     textOut = "Hah";
